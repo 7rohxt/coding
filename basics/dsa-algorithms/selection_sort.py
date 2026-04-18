d45cf51a37d5cc49aa6206edba32d650c5d32f1d
Rohit Karthick <rkbeo5@gmail.com>
2026-04-19 01:32:57 +0530

    dsa-algorithms: selection sort

---
+++ basics/dsa-algorithms/selection_sort.py
@@ -0,0 +1,17 @@
+arr = [1, 4, 6, 3, 2]
+
+def selectionSort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        min_index = i
+
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    return arr
+
+print(selectionSort(arr))
