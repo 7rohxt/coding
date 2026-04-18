59f094745c0903fe9de593ad5a03b53dc716e3eb
Rohit Karthick <rkbeo5@gmail.com>
2026-04-19 03:16:33 +0530

    dsa-algorithms: trees dfs with recursion

---
+++ basics/dsa-algorithms/trees/all_dfs.py
@@ -0,0 +1,62 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.val)
+
+
+# Create nodes
+A = TreeNode(1)
+B = TreeNode(2)
+C = TreeNode(3)
+D = TreeNode(4)
+E = TreeNode(5)
+F = TreeNode(10)
+
+# Connect nodes
+A.left = B
+A.right = C
+
+B.left = D
+B.right = E
+
+C.right = F   # <-- important correction
+
+# Root
+root = A
+
+print(root)
+
+
+def preOrder(node):
+    if not node:
+        return
+    
+    print(node)
+    preOrder(node.left)
+    preOrder(node.right)
+
+# print(preOrder(A))
+
+def inOrder(node):
+    if not node:
+        return
+    
+    inOrder(node.left)
+    print(node)
+    inOrder(node.right)
+
+# print(inOrder(A))
+
+def postOrder(node):
+    if not node:
+        return
+    
+    inOrder(node.left)
+    inOrder(node.right)
+    print(node)
+
+# print(postOrder(A))
