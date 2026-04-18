25d76780261f8e085679ffbb9418f38b431cc0a9
Rohit Karthick <rkbeo5@gmail.com>
2026-04-19 01:20:32 +0530

    dsa-algorithms: insertion sort

---
+++ basics/dsa-algorithms/insertion_sort.py
@@ -0,0 +1,16 @@
+arr = [1, 4, 6, 3, 2]
+
+def insertionSort(arr):
+    n = len(arr)
+
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+            else:
+                break
+    
+    return arr
+
+print(insertionSort(arr))
+        
